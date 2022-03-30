4991db91a23dc0d1b57f2ce7f7b8d9c6802ec88e
Hwajiin <borderline0px@gmail.com>
2022-03-31 01:09:49 +0900

    📝 리포츠 뉴스 정리

---
+++ src/data/news.js
@@ -0,0 +1,72 @@
+const news = [
+  {
+    id: 0,
+    date: "21.12.10",
+    title: "건강한 즐거움이 만드는 색다른 경험 ‘리포츠’",
+    publisher: "SK MEDIA",
+    url: "#",
+  },
+  {
+    id: 1,
+    date: "21.11.30",
+    title: "나눔엔젤스, '2021 인천 라이징 스타 육성 프로그램' 데모데이 성료",
+    publisher: "데일리경제",
+    url: "#",
+  },
+  {
+    id: 2,
+    date: "21.11.29",
+    title:
+      "리포츠, '2021 I-STARTUP'서 AI큐레이션 스포츠 강습 서비스 '세모스' 알린다",
+    publisher: "CNBNEWS",
+    url: "#",
+  },
+  {
+    id: 3,
+    date: "21.11.16",
+    title:
+      "리포츠, '2021 I-STARTUP'서 AI큐레이션 스포츠 강습 서비스 '세모스' 알린다",
+    publisher: "AVING 뉴스",
+    url: "#",
+  },
+  {
+    id: 4,
+    date: "21.09.24",
+    title: `[스타트업] "취미운동 맞춤 1대1 O2O 플랫폼" 변민지 리포츠 대표`,
+    publisher: "프라임경제",
+    url: "#",
+  },
+  {
+    id: 5,
+    date: "21.08.27",
+    title:
+      "인천청년창업사관학교, 11기 기업 대상 '스타트업 언론 홍보 전략' 교육 진행",
+    publisher: "프라임경제",
+    url: "#",
+  },
+  {
+    id: 6,
+    date: "21.07.19",
+    title:
+      "MBA 학생은 어떻게 하루를 보낼까? - Social Entrepreneurship MBA 21학번 변민지 원우의 하루 일과를",
+    publisher: "KAIST",
+    url: "#",
+  },
+  {
+    id: 7,
+    date: "21.05.25",
+    title: "인천 여성창업가, 경인여대 예비 창업가들과 네트워킹 가져",
+    publisher: "머니투데이",
+    url: "#",
+  },
+  {
+    id: 8,
+    date: "21.05.18",
+    title:
+      "여기서 탄생한 기업가치 총액만 3조? 핫한 기업들이 더해져 더욱 기대된다는 이곳",
+    publisher: "Dispatch",
+    url: "#",
+  },
+];
+
+export default news;
