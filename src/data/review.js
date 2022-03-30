074a2ff62d2b66b860b1cac92b06c4857b89e369
Hwajiin <borderline0px@gmail.com>
2022-03-31 01:10:03 +0900

    📝 세모스 리뷰 정리

---
+++ src/data/review.js
@@ -0,0 +1,50 @@
+import review001 from "../assets/images/review/review-1.png";
+import review002 from "../assets/images/review/review-2.png";
+import review003 from "../assets/images/review/review-3.png";
+
+const review = [
+  {
+    id: 0,
+    img: review001,
+    name: "세모스 크루",
+    comment:
+      "처음 접하는 프리다이빙임에도 불구하고 강사님의 친절한 설명덕에 이해하기 쉬웠고 반복적으로 강습생을 위해 자세한 부가설명이 너무 좋았습니다 ~",
+  },
+  {
+    id: 1,
+    img: review002,
+    name: "세모스 강사",
+    comment:
+      "신규 수강생 문제로 고민이 많았는데, 세모스 덕분에 좋은 인연이 닿을 수 있어 항상 감사합니다. ^^",
+  },
+  {
+    id: 2,
+    img: review003,
+    name: "세모스 크루",
+    comment:
+      "강습 예약하는게 항상 번거로웠는데 간편하게 예약할 수 있어서 참 좋아요. 세모스 도장깨기로 다른 종목들도 경험하고 싶네요. ㅎㅎㅎ",
+  },
+  {
+    id: 3,
+    img: review001,
+    name: "세모스 크루",
+    comment:
+      "처음 접하는 프리다이빙임에도 불구하고 강사님의 친절한 설명덕에 이해하기 쉬웠고 반복적으로 강습생을 위해 자세한 부가설명이 너무 좋았습니다 ~",
+  },
+  {
+    id: 4,
+    img: review002,
+    name: "세모스 강사",
+    comment:
+      "신규 수강생 문제로 고민이 많았는데, 세모스 덕분에 좋은 인연이 닿을 수 있어 항상 감사합니다. ^^",
+  },
+  {
+    id: 5,
+    img: review003,
+    name: "세모스 크루",
+    comment:
+      "강습 예약하는게 항상 번거로웠는데 간편하게 예약할 수 있어서 참 좋아요. 세모스 도장깨기로 다른 종목들도 경험하고 싶네요. ㅎㅎㅎ",
+  },
+];
+
+export default review;
